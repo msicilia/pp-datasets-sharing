0707f76325ce794c12d6f89250388e7a66f4de36
Miguel-Angel Sicilia <msicilia@gmail.com>
2021-04-12 22:34:19 +0200

    First commit

---
+++ contracts/DatasetBidRegistry.sol
@@ -0,0 +1,112 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/access/Ownable.sol";
+
+
+/** @title A registry of dataset bids. */
+contract DatasetBidRegistry is Ownable {
+   
+   /** Organizations may issue bids for datasets.
+   * 
+   *   A bid has an expiry date, plus an amount which is the price willing to pay, and
+   *   a tag which serves for indexing (e.g. references to concepts in knowledge graphs or terminologies).
+   * 
+   *   The spec of the dataset is not stored in the smart contract, but instead
+   *   a hash to a claim file is included. 
+   *   A claim file is just a JSON document with:
+   *   - Another IPFS hash to the dataset spec file. 
+   *   - A digital signature of the hash (ideally associated to a certificate of the bidder). 
+   *   
+   *   The dataset spec file is a JSON document with the complete description of the 
+   *   desired dataset, which may be verbose and detailed.
+   *
+   *   Claim files can be considered similar to the concept of a Ricardian contract, since the
+   *   bidder commits to pay the specified amount iif the specs are met. 
+   */
+   struct Bid{
+       string expiry_date; // Standard human readable date, ISO 8601 UTC
+       bytes hash_spec;    // IPFS address to the claim file. 
+       uint amount;        // Amount in Ether for the bid.
+       string tag;	      // Keyword
+   }
+
+   /**
+    * An offer is issued by a dataset provider in response to an (active) bid. 
+    */
+   struct Offer{
+      address offerer;  // address of the offerer. .  
+      address bidder;   // bidder and bid_number identify the bid.
+      uint bid_number;
+      uint value;       // price proposed, in Ether.
+      bool completed;
+   }
+
+   // Notification for interested parties of new bids.
+   event BidRegistered(
+          address who,
+          uint position           
+   );
+
+   // Notification for interested parties of new offers.
+   event OfferRegistered(
+          address from,
+          address to,
+          uint bid_number,   
+          uint offer_number
+   );
+
+   // The bids per bidder.
+   mapping (address => Bid[]) public bids;
+   // The offers received per bid. Associates a (bidder address, bid no) to a list of offers. 
+   mapping (address => mapping (uint => Offer[])) public offers;
+
+
+  /** @dev Register a temporary dataset bid.
+    * @param date Expiration date of the bid.
+    * @param hash_spec Hash to the spec of the bid.
+    * @param amount Amount to be paid for the dataset.
+    */
+  function register(string memory date,
+                    bytes memory hash_spec,
+                    uint amount,
+                    string memory tag) public {
+      bids[msg.sender].push(Bid(date, hash_spec, amount, tag));
+      emit BidRegistered(msg.sender, bids[msg.sender].length-1);
+  }
+
+  /**
+   * Gets info of a particular bid.
+   * @param bidder The address of the bidder.
+   * @param pos The position in the list of bids by the bidder (included in BidRegistered events). 
+   */
+  function bidinfo(address bidder, uint pos) public view 
+            returns (string memory expiry_date, bytes memory hash_spec, uint amount, string memory tag){
+      Bid memory b = bids[bidder][pos];
+     return (b.expiry_date, b.hash_spec, b.amount, b.tag);
+  }
+  /**
+   * An offer is a response to an (active) bid for a dataset.
+   * 
+   * Expiry dates of bids are considered informative and not checked inside the contract,
+   * it is expected that clients do the check. 
+   */
+   function offer(address recp, uint bidno, uint price) public {
+         offers[recp][bidno].push(Offer(msg.sender, recp, bidno, price, false));
+         emit OfferRegistered(msg.sender, recp, bidno, offers[recp][bidno].length - 1);
+   }
+
+   /**
+    * The bidder is able to accept and finalize an offer. 
+    * 
+    */
+   function finalize(address recp, uint bidno, uint offerno) public payable{
+        assert(msg.sender == offers[recp][bidno][offerno].bidder);
+        assert(msg.value == offers[recp][bidno][offerno].value);
+        offers[recp][bidno][offerno].completed = true;
+        // Transfer the value.
+	     offers[recp][bidno][offerno].offerer.transfer(this.balance);
+   } 
+
+}
---
+++ tests/register_bid_test.py
@@ -0,0 +1,62 @@
+import hashlib
+import base58
+from web3.auto import w3
+from eth_account.messages import encode_defunct
+import json
+from ecdsa import SigningKey 
+from brownie import DatasetBidRegistry, accounts
+import pytest
+import os
+import datetime as dt
+
+
+@pytest.fixture
+def registry():
+    '''Deploys a singleton dataset bid registry
+    '''
+    return accounts[0].deploy(DatasetBidRegistry)
+
+
+def create_ipfs_multihash(content: bytes):
+    ''' Helper function to create IPFS multihashes.
+        IPFS uses multihashes with the following format:
+        base58(<varint hash function code><varint digest size in bytes><hash function output>)
+    '''
+    content_hash = hashlib.sha256()
+    content_hash.update(content)
+    function = int(18).to_bytes(1, byteorder='big')
+    size = content_hash.digest_size.to_bytes(1, byteorder='big')
+    return base58.b58encode(function + size + content_hash.digest())
+
+def create_signed_claim_hash(dataset_spec_file : str):
+    ''' Assembles a claim for a dataset spec file.
+        A claim is just a hash from the dataset spec file digitally signed.
+    '''
+    # The hash from the dataset spec need to be converted to string for JSON serialization.
+    data_hash = create_ipfs_multihash(bytes(dataset_spec_file, 'utf-8'))
+    # Example signing of the dataset spec hash, it real setting could be associated to a certificate. 
+    sk = SigningKey.generate()
+    signature = sk.sign(data_hash)
+    # Build the claim, transform to string and hash.
+    claim = {'dataset_spec_hash': str(data_hash),
+             'signature': str(signature)}
+    return create_ipfs_multihash(bytes(json.dumps(claim), 'utf-8'))
+
+def test_ownership(registry):
+    ''' Tests the contract is Ownable (OpenZeppelin libraries)
+    '''
+    assert(accounts[0] == registry.owner())
+
+def test_register(registry):
+    with open(os.path.join(os.path.dirname(__file__), "sample_dataset_spec.json"), 'r') as f:
+         spec_file = json.load(f)
+    timestamp = (dt.datetime.now() +dt.timedelta(days=60)).isoformat()
+    tx = registry.register(timestamp, 
+                        create_signed_claim_hash(str(spec_file)),
+                        1000,
+                        "SNOMED_ID:394732004", 
+                        {'from': accounts[1]}
+                      )
+    expiry_date, hash_spec, amount, tag = registry.bidinfo(tx.events[0]['who'], tx.events[0]['position'])
+    assert(expiry_date==timestamp)
+    assert(amount==1000)    
